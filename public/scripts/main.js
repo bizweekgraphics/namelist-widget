58ea1391bf83f702ac85dfa5717eeaa2b2349291
Toph Tucker <tophtucker@gmail.com>
2015-04-02 16:21:57 -0400

    Hover tiles, preview on property rollover, lookin ok

---
+++ public/scripts/main.js
@@ -0,0 +1,31 @@
+d3.json('static/names.json', function(error, names) {
+
+  var nameList = d3.select("#names").selectAll(".name")
+      .data(names)
+    .enter()
+      .append("div")
+      .classed("name", true)
+      .append("div")
+      .classed("name-inner", true);
+
+  var properties = Object.keys(names[0]);
+
+  properties.forEach(function(value, index) {
+    nameList.append("p")
+      .attr("data-property", value)
+      .text(function(d) { return d[value]; });
+  })
+
+  d3.select("#properties").selectAll(".property")
+      .data(properties)
+      .enter()
+      .append("div")
+      .classed("property", true)
+      .style("width", (100/properties.length)+"%")
+      .text(function(d) { return d; })
+      .on("mouseover", function(d) {
+        d3.selectAll('.name p').classed('selected', false);
+        d3.selectAll('.name p[data-property="'+d+'"]').classed('selected', true);
+      });
+
+})
