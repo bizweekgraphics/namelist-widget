3e4acead35e19f39c67023fb4318e7ff97ab96bd
Toph Tucker <tophtucker@gmail.com>
2015-04-03 17:31:15 -0400

    Adding color-coding

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,12 @@
+var config = {
+  "key": "Name",
+  "scales": {
+    "Name": function() { return "#ffffff"; },
+    "Age": d3.scale.linear().domain([0,100]).range(["#ffffff", "#ff00ff"]).clamp(true),
+    "State": d3.scale.ordinal().range(["#2800D7", "#FA1E64", "#FF6564", "#5C42AB", "#00B9E7", "#00DC3C", "#FB8E1E"])
+  }
+}
+
 d3.json('static/names.json', function(error, names) {
 
   var nameList = d3.select("#names").selectAll(".name")
@@ -27,10 +36,11 @@ d3.json('static/names.json', function(error, names) {
       .on("mouseover", function(d) {
         d3.selectAll('.name p').classed('selected', false);
         d3.selectAll('.name p[data-property="'+d+'"]').classed('selected', true);
+        nameListInner.style("background", function(dd) { return config.scales[d] ? config.scales[d](dd[d]) : "#ffffff"; });
       })
       .on("click", function(d) {
         nameList.sort(function(a, b) {
-          return a[d] < b[d] ? -1 : a[d] > b[d] ? 1 : a[d] >= b[d] ? 0 : NaN;
+          return a[d] < b[d] ? -1 : a[d] > b[d] ? 1 : a[d] >= b[d] ? (a["Name"] < b["Name"] ? -1 : a["Name"] > b["Name"] ? 1 : 0) : NaN;
         });
       });
 
